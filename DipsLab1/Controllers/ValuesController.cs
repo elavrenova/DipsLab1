8505fb03af0470336f0d4a07227c60a72db98419
Elizaveta Lavrenova <e.lavr95@gmail.com>
2017-09-24 11:58:06 +0300

    Add project files.

---
+++ DipsLab1/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DipsLab1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string GetString(int id, [FromQuery] string name)
+        {
+            if (String.IsNullOrWhiteSpace(name))
+                name = "Somebody";
+            if (id == 0)
+                return name + " doesn't have any cats";
+            else
+                return name + " has " + id + " cats";
+        }
+
+
+    }
+}
